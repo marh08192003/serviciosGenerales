08199c658c485d7614f489a5c8dc453e79669efa
marh08192003 <riverita470@gmail.com>
2024-12-26 14:43:21 -0500

    Initial commit

---
+++ src/main/java/co/edu/uceva/serviciosGenerales/ServiciosGeneralesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.serviciosGenerales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosGeneralesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosGeneralesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uceva/serviciosGenerales/ServiciosGeneralesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.serviciosGenerales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosGeneralesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
